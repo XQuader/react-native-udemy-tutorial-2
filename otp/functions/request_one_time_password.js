400add005c9298a998bdc0e63a7d570470ba085b
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-09-04 17:14:48 +0300

    Firebase: add 'RequestOneTimePassword.js'

--- otp/functions/request_one_time_password.js
+++ otp/functions/request_one_time_password.js
@@ -1,3 +1,27 @@
-module.exports = (req, res) => {
+const admin = require('firebase-admin');
+const twilio = require('./twilio');
 
+module.exports = (req, res) => {
+  const phone = String(req.body.phone).replace(/[^\d]/g,'');
+
+  if (!phone) {
+    return res.status(422).send({ error: 'You must provide a phone number' });
+  }
+
+  admin.auth().getUser(phone)
+    .then(user => {
+      const code = Math.floor(Math.random()*8999 + 1000);
+
+      twilio.messages.create({
+        body: 'Your code is ' + code,
+        to: '+' + phone,
+        from: '+14808000219'
+      }, (error) => {
+          if (error) { return res.status(422).send(error); }
+
+          return admin.database().ref('users/' + phone)
+            .update({ code, codeValid: true }, () => res.send({ success: true }))
+      })
+    })
+    .catch(error => res.status(422).send(error));
 };
