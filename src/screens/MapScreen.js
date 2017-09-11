d7b7eac250202a742cf0d995ad5b3a8b62e1082f
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-09-11 17:10:23 +0300

    Draw MapView Draw spinner until current gps coords are acquired

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, View } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+  state = {
+    mapLoaded: false,
+    region: {
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }
+  };
+
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(position => {
+        const { region } = this.state;
+        const { latitude, longitude } = position.coords;
+        const newRegion = { ...region, latitude, longitude };
+
+        this.setState({ mapLoaded: true, region: newRegion });
+      }, error => this.setState({ mapLoaded: true }), {
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 1000
+      }
+    );
+  }
+
+  handleRegionChange = region => {
+    this.setState({ region });
+  };
+
   render() {
+    if (!this.state.mapLoaded) {
+      return <ActivityIndicator size={80} style={styles.spinner}/>
+    }
+
     return (
-      <View style={styles.container}>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={this.state.region}
+          onRegionChangeComplete={this.handleRegionChange}
+        />
       </View>
     );
   }
@@ -13,7 +51,12 @@ class MapScreen extends Component {
 
 const styles = {
   container: {
-
+    flex: 1
+  },
+  spinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 };
 
