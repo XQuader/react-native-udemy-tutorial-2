19391b1f1af60882c9b23b421bf80b3802a59531
Semyon Balakhnin <sbalakhnin@exadel.com>
2017-09-12 17:54:05 +0300

    Populate DeckScreen with Job Cards

---
+++ src/components/Deck.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import {
+  Animated,
+  PanResponder,
+  Dimensions,
+  Text
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.5 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
+
+class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {},
+    renderNoMoreCards: () => <Text>No more Cards to show!</Text>,
+    keyProp: 'id'
+  };
+
+  state = {
+    index: 0
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.positionActiveCard = new Animated.ValueXY();
+    this.positionRestCards = new Animated.ValueXY({ x: 0, y: 10 });
+    this.panResponder = new PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        this.positionActiveCard.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('Right');
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('Left');
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.data !== this.props.data) {
+      this.setState({ index: 0 });
+    }
+  }
+
+  forceSwipe(direction) {
+    const x = 10 + (direction === 'Right' ? SCREEN_WIDTH : -SCREEN_WIDTH);
+
+    Animated.timing(this.positionActiveCard, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete = (direction) => {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'Right' ? onSwipeRight(item) : onSwipeLeft(item);
+    Animated.timing(this.positionRestCards, {
+      toValue: { x: 0, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => {
+      this.positionActiveCard.setValue({ x: 0, y: 0 });
+      this.positionRestCards.setValue({ x: 0, y: 10 });
+      this.setState({ index: this.state.index + 1 });
+    });
+
+  };
+
+  resetPosition() {
+    Animated.spring(this.positionActiveCard, {
+      toValue: {
+        x: 0,
+        y: 0
+      }
+    }).start();
+  }
+
+  getCardStyle() {
+    const rotate = this.positionActiveCard.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
+    return {
+      ...this.positionActiveCard.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
+  renderCards() {
+    if (this.state.index >= this.props.data.length) {
+      return this.props.renderNoMoreCards();
+    }
+
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) { return null; }
+
+      if (i === this.state.index) {
+        return (
+          <Animated.View
+            key={item[this.props.keyProp]}
+            style={[this.getCardStyle(), styles.cardStyle, { zIndex: 1 }]}
+            {...this.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        )
+      }
+
+      return (
+        <Animated.View key={item[this.props.keyProp]} style={[styles.cardStyle, { top: 10 * (i - this.state.index) }]}>
+          {this.props.renderCard(item)}
+        </Animated.View>
+      );
+    }).reverse();
+  }
+
+  render() {
+    return (
+      <Animated.View style={[this.positionRestCards.getLayout(), this.props.style]}>
+        {this.renderCards()}
+      </Animated.View>
+    );
+  }
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH,
+    zIndex: 0
+  }
+};
+
+export default Deck;
--- src/screens/DeckScreen.js
+++ src/screens/DeckScreen.js
@@ -1,20 +1,88 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { Card } from 'react-native-elements';
+import { Dimensions, Text, View, Platform } from 'react-native';
+import Deck from '../components/Deck';
+import { MapView } from 'expo';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class DeckScreen extends Component {
+  renderCard(job) {
+    const { latitude, longitude, company, jobtitle, formattedRelativeTime, snippet, url } = job;
+    const region = {
+      latitude,
+      longitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    };
+
+    return (
+      <Card
+        containerStyle={{ borderRadius: 10, width: SCREEN_WIDTH * 0.92, height: SCREEN_HEIGHT - 165, flexDirection: 'row' }}
+        title={jobtitle}
+      >
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={region}
+          scrollEnabled={false}
+          cacheEnabled={Platform.OS === 'android' ? true : false}
+        />
+        <View style={styles.detailsWrapper}>
+          <Text>{company}</Text>
+          <Text>{formattedRelativeTime}</Text>
+        </View>
+        <Text>
+          {snippet.replace(/<\/?b>/gi, '')}
+        </Text>
+      </Card>
+    )
+  }
+
+  onSwipeRight(job) {
+    console.log('job liked: ' + job.jobtitle);
+  }
+
+  onSwipeLeft(job) {
+    console.log('job disliked: ' + job.jobtitle);
+  }
+
+  renderNoMoreCards() {
+    return (
+      <Card
+        containerStyle={{ borderRadius: 10, width: SCREEN_WIDTH * 0.92, height: SCREEN_HEIGHT - 165 }}
+        title="No more jobs"
+      />
+    )
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>DeckScreen</Text>
-      </View>
+      <Deck
+        data={this.props.jobs}
+        keyProp='jobkey'
+        renderCard={this.renderCard}
+        renderNoMoreCards={this.renderNoMoreCards}
+        onSwipeRight={this.onSwipeRight}
+        onSwipeLeft={this.onSwipeLeft}
+      />
     );
   }
 }
 
 const styles = {
-  container: {
-
+  detailsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+    marginTop: 10
   }
 };
 
-export { DeckScreen };
+const mapStateToProps = ({ jobs }) => ({
+  jobs: jobs.results
+});
+const reduxed = connect(mapStateToProps)(DeckScreen);
+
+export { reduxed as DeckScreen };
